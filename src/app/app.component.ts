b9c818d0fb5c0eb560de14d183f65eee56f7a873
razorsyntax <biomancefilms@hotmail.com>
2024-09-04 12:47:47 -0500

    remove modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'fundamentally-fleek';
