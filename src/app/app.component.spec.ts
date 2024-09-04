3bf29e2c8988cdbe92967552203dd6223dee7b07
razorsyntax <biomancefilms@hotmail.com>
2024-09-04 12:21:19 -0500

    updated names and budget

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fundamentally-investor-site'`, () => {
+  it(`should have as title 'fundamentally-fleek'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fundamentally-investor-site');
+    expect(app.title).toEqual('fundamentally-fleek');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fundamentally-investor-site app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('fundamentally-fleek app is running!');
   });
 });
