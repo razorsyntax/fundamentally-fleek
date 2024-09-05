11a706281c84fa5ef15e1620cc524901a4b240cf
razorsyntax <biomancefilms@hotmail.com>
2024-09-05 14:33:11 -0500

    updated navbar

--- src/app/library/components/header/header.component.ts
+++ src/app/library/components/header/header.component.ts
@@ -1,14 +1,53 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule, ViewportScroller } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent { }
+export class HeaderComponent implements OnInit {
+  isNavbarVisible = false;
+  isMobileScreen: boolean = false;
+  private screenSizeSubscription!: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private viewportScroller: ViewportScroller,
+  ) {}
+
+  ngOnInit() {
+    this.route.fragment.subscribe(() => {
+      this.scrollToFragment();
+    });
+  }
+
+  onLinkClick(event: Event, fragment: string) {
+    event.preventDefault();
+    this.viewportScroller.scrollToAnchor(fragment);
+    this.closeNavbar();
+  }
+
+
+  private scrollToFragment() {
+    const fragment = this.route.snapshot.fragment;
+    if (fragment) {
+      this.viewportScroller.scrollToAnchor(fragment);
+    } else {
+      this.viewportScroller.scrollToPosition([0, 0]);
+    }
+  }
+
+  closeNavbar() {
+    this.isNavbarVisible = false;
+  }
+
+ }
