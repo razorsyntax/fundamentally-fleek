edfc789674edb6e9be6ced0ed9780e3f1425b27b
razorsyntax <biomancefilms@hotmail.com>
2025-03-08 10:54:51 -0600

    refund stuff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { RefundsComponent } from './refunds/refunds.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: 'terms-and-conditions', component: TermsAndConditionsComponent },
+  { path: 'refunds', component: RefundsComponent },
   // { path: '**', component: PageNotFoundComponent },
 ];
---
+++ src/app/refunds/refunds.component.ts
@@ -0,0 +1,14 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "../library/components/header/header.component";
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-refunds',
+  standalone: true,
+  imports: [CommonModule, RouterLink, HeaderComponent],
+  templateUrl: './refunds.component.html',
+  styleUrls: ['./refunds.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RefundsComponent {}
